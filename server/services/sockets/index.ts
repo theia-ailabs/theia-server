585771dadeaf195b62c7efd03d3ef08f4555d590
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-18 20:56:30 +0200

    add socket.volatile

--- server/services/sockets/index.ts
+++ server/services/sockets/index.ts
@@ -37,7 +37,7 @@ const searchUserSocket = (socket: Socket): void => {
 };
 
 const askTheiaSocket = (socket: Socket): void => {
-  socket.on("askTheia", async (question: string) => {
+  socket.volatile.on("askTheia", async (question: string) => {
     if (question) {
       console.log(question);
       const response = await askChatGPT(question);
@@ -45,12 +45,12 @@ const askTheiaSocket = (socket: Socket): void => {
         text: response,
         audio: new AudioBuffer({ length: 0, sampleRate: 0 }),
       };
-      socket.emit("theiaRes", res);
+      socket.volatile.emit("theiaRes", res);
       res.audio = await getSpeech(res.text as string);
-      socket.emit("theiaRes", res);
+      socket.volatile.emit("theiaRes", res);
     } else {
-      const msgErr = `❌ ERROR: Input msg wrong.`;
-      socket.emit("theiaRes", msgErr);
+      const msgErr = `❌ ERROR: Input msg undefined.`;
+      socket.volatile.emit("theiaRes", msgErr);
       console.log("askTheiaSocket", msgErr);
     }
   });
