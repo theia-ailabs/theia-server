4d40a47752be3d84eb528fadc10f503a104fc10c
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-20 03:14:01 +0200

    add getSpeechUrl() service

--- server/services/sockets/index.ts
+++ server/services/sockets/index.ts
@@ -47,6 +47,7 @@ const askTheiaSocket = (socket: Socket): void => {
       };
       socket.volatile.emit("theiaRes", res);
       res.audio = await getSpeechUrl(res.text);
+      console.log(res.audio);
       socket.volatile.emit("theiaRes", res);
     } else {
       const msgErr = `❌ ERROR: Input msg undefined.`;
