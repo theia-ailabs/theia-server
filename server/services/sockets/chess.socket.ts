5e05294493050c9ecd8835f650199205d9f0a095
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-22 17:05:47 +0200

    add lichess sockets

--- server/services/sockets/chess.socket.ts
+++ server/services/sockets/chess.socket.ts
@@ -2,8 +2,7 @@ import { Socket } from "socket.io";
 import { getLichessUser } from "../chess/auth";
 
 export const lichessAccountSocket = (socket: Socket): void => {
-  socket.on("lichessAccount", (token: string) => {
+  socket.on("lichessAccount", async (token: string) => {
     socket.emit("lichessAccountRes", await getLichessUser(token));
-    });
   });
 };
